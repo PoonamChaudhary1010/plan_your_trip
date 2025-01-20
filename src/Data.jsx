7cf0b2b317fe350970a355007c3b4bd2a165c705
PoonamChaudhary1010 <2003poonamchaudhary@gmail.com>
2025-01-20 23:05:29 +0530

    adding all files

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const data=[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra, nestled in the northern state of Uttar Pradesh, India, is a city renowned for its historical and architectural significance. Dominating its skyline is the magnificent Taj Mahal, an ethereal white marble mausoleum built by Emperor Shah Jahan in memory of his beloved wife, Mumtaz Mahal. This architectural marvel, with its intricate carvings and breathtaking symmetry, stands as a testament to eternal love and is considered one of the New Seven Wonders of the World.",
+        image:"https://imgs.search.brave.com/sIW8YZwP8dGKMwxkkRqccHlcGxQTPlbg6TI2pDhFIHY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8xLzFkL1Rh/al9NYWhhbF8lMjhF/ZGl0ZWQlMjkuanBl/Zy81MTJweC1UYWpf/TWFoYWxfJTI4RWRp/dGVkJTI5LmpwZWc",
+        price:"Rs.35,758"
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur, the vibrant capital of Rajasthan, India, is often referred to as the Pink City due to the distinctive color of its buildings. The city is home to majestic palaces, such as the City Palace, a blend of Rajput, Mughal, and European architectural styles. The Hawa Mahal, or Palace of Winds, with its intricate latticework, is another iconic landmark. Amber Fort, perched on a hill, offers panoramic views and showcases the opulence of the Rajput era. Jaipur's bustling bazaars are a treasure trove of traditional crafts, textiles, and jewelry. The city's vibrant festivals, such as the Jaipur Literature Festival and Teej, add to its cultural charm.",
+        image:"https://imgs.search.brave.com/SGH6C5Cg7heKc3R9El5AeRjMHz195uX2OBWsNjHDuVc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi80LzQxL0Vh/c3RfZmFjYWRlX0hh/d2FfTWFoYWxfSmFp/cHVyX2Zyb21fZ3Jv/dW5kX2xldmVsXyUy/OEp1bHlfMjAyMiUy/OV8tX2ltZ18wMS5q/cGcvNTEycHgtRWFz/dF9mYWNhZGVfSGF3/YV9NYWhhbF9KYWlw/dXJfZnJvbV9ncm91/bmRfbGV2ZWxfJTI4/SnVseV8yMDIyJTI5/Xy1faW1nXzAxLmpw/Zw",
+        price:"Rs.82,560"
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa, located on the western coast of India, is a tropical paradise famed for its stunning beaches, vibrant nightlife, and rich cultural heritage. A former Portuguese colony, Goa beautifully blends Indian and Portuguese influences in its architecture, cuisine, and festivals. The state's legendary nightlife, coupled with its culinary scene featuring fresh seafood and traditional Goan dishes, promises an unforgettable experience for every visitor.",
+        image:"https://imgs.search.brave.com/MbgPHdVRIBN6tRkixOVhqTiLk2BWC5hdbEr4IO2SNaI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTcx/MTQ0NTM4L3Bob3Rv/L2dvYS1wYWxvbGVt/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz14cjlvaE56QmIw/cEx4aDJRbUJVWlFU/XzZTNWY0R2Z6aldV/ZGhLNGZmZi1rPQ",
+        price:"Rs.29,695"
+    },
+    {
+        id:4,
+        name:"Goa",
+        info:"Goa, located on the western coast of India, is a tropical paradise famed for its stunning beaches, vibrant nightlife, and rich cultural heritage. A former Portuguese colony, Goa beautifully blends Indian and Portuguese influences in its architecture, cuisine, and festivals. The state's legendary nightlife, coupled with its culinary scene featuring fresh seafood and traditional Goan dishes, promises an unforgettable experience for every visitor.",
+        image:"https://imgs.search.brave.com/MbgPHdVRIBN6tRkixOVhqTiLk2BWC5hdbEr4IO2SNaI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTcx/MTQ0NTM4L3Bob3Rv/L2dvYS1wYWxvbGVt/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz14cjlvaE56QmIw/cEx4aDJRbUJVWlFU/XzZTNWY0R2Z6aldV/ZGhLNGZmZi1rPQ",
+        price:"Rs.29,695"
+    },
+    {
+        id:5,
+        name:"Agra",
+        info:"Agra, nestled in the northern state of Uttar Pradesh, India, is a city renowned for its historical and architectural significance. Dominating its skyline is the magnificent Taj Mahal, an ethereal white marble mausoleum built by Emperor Shah Jahan in memory of his beloved wife, Mumtaz Mahal. This architectural marvel, with its intricate carvings and breathtaking symmetry, stands as a testament to eternal love and is considered one of the New Seven Wonders of the World.",
+        image:"https://imgs.search.brave.com/sIW8YZwP8dGKMwxkkRqccHlcGxQTPlbg6TI2pDhFIHY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8xLzFkL1Rh/al9NYWhhbF8lMjhF/ZGl0ZWQlMjkuanBl/Zy81MTJweC1UYWpf/TWFoYWxfJTI4RWRp/dGVkJTI5LmpwZWc",
+        price:"Rs.35,758"
+    },
+    {
+        id:6,
+        name:"Jaipur",
+        info:"Jaipur, the vibrant capital of Rajasthan, India, is often referred to as the Pink City due to the distinctive color of its buildings. The city is home to majestic palaces, such as the City Palace, a blend of Rajput, Mughal, and European architectural styles. The Hawa Mahal, or Palace of Winds, with its intricate latticework, is another iconic landmark. Amber Fort, perched on a hill, offers panoramic views and showcases the opulence of the Rajput era. Jaipur's bustling bazaars are a treasure trove of traditional crafts, textiles, and jewelry. The city's vibrant festivals, such as the Jaipur Literature Festival and Teej, add to its cultural charm.",
+        image:"https://imgs.search.brave.com/SGH6C5Cg7heKc3R9El5AeRjMHz195uX2OBWsNjHDuVc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi80LzQxL0Vh/c3RfZmFjYWRlX0hh/d2FfTWFoYWxfSmFp/cHVyX2Zyb21fZ3Jv/dW5kX2xldmVsXyUy/OEp1bHlfMjAyMiUy/OV8tX2ltZ18wMS5q/cGcvNTEycHgtRWFz/dF9mYWNhZGVfSGF3/YV9NYWhhbF9KYWlw/dXJfZnJvbV9ncm91/bmRfbGV2ZWxfJTI4/SnVseV8yMDIyJTI5/Xy1faW1nXzAxLmpw/Zw",
+        price:"Rs.82,560"
+    },
+]
+
+export default data;
